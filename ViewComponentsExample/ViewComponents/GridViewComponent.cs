eff8352eb58be52e6aaccb11158eb0b79e9a8168
GrayJunZi <GrayJunZi@outlook.com>
2025-01-09 14:18:56 +0800

    096. 创建视图组件

---
+++ ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponentsExample.ViewComponents;
+
+public class GridViewComponent : ViewComponent
+{
+   public Task<IViewComponentResult> InvokeAsync()
+   {
+       // 调用分部视图
+       // Views\Shared\Components\Grid\Default.cshtml
+       return View();
+   }
+}
